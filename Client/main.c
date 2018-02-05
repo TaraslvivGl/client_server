f227e68027fc8f6562a205ced9090c5bdec82849
Taras Shevchuk <taras.shevchuk@globallogic.com>
2018-02-05 13:42:32 +0200

    eddit code due to comments

--- Client/main.c
+++ Client/main.c
@@ -1,57 +1,125 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
 #include <arpa/inet.h>
-#include<unistd.h>
-
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define SERVER "127.0.0.1"
-#define BUFLEN 512
-#define PORT 8888
+#define BUFLEN (512)
+#define PORT (8888)
+#define DATA_COUTN (1)
 
-void errorLog(char *s)
+void errorLog(const char* const s, const int socket, char** data)
 {
+    int i;
+    for (i = 0; i < DATA_COUTN; i++)
+    {
+        free(data[i]);
+    }
+    free(data);
+
+    if (socket > 0)
+    {
+        close(socket);
+    }
     perror(s);
     exit(1);
+}
+
+void exitOnRequest(char** data, const int socket)
+{
+    int i;
+    for (i = 0; i < DATA_COUTN; i++)
+    {
+        if(data[i] != NULL)
+        {
+            free(data[i]);
+        }
+    }
+    free(data);
+
+    printf("\nConnection closed\n");
+
+    if (socket > 0)
+    {
+        close(socket);
+    }
+
+    exit(0);
 }
 
 int main(void)
 {
     struct sockaddr_in server;
-    int sockedFd;
-    char buf[BUFLEN];
-    char message[BUFLEN];
+    int servLen = sizeof(server);
+    int sockedFd = -1;
+    char* message = malloc(BUFLEN);
+    char** data = malloc(DATA_COUTN * sizeof(char*));
+    int ret = 0;
+
+    data[0] = message;
+
+    /*Ctrl+C exit*/
+    void exitHadler()
+    {
+        exitOnRequest(data, sockedFd);
+    }
+    signal(SIGINT, exitHadler);
 
     if ((sockedFd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
     {
-        errorLog("Error socket initialization");
+        errorLog("Error socket initialization", sockedFd, data);
     }
 
-    memset((char *) &server, 0, sizeof(server));
+    memset(&server, 0, sizeof(server));
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
 
+    /*connection to server*/
     if (connect(sockedFd, (struct sockaddr *)&server, sizeof(server)) < 0)
     {
-        errorLog("Cannot connect to server");
+        errorLog("Cannot connect to server\n", sockedFd, data);
     }
 
     if(!inet_aton(SERVER, &server.sin_addr))
     {
-        errorLog("Invalid IP Address \n");
+        errorLog("Invalid IP Address \n", sockedFd, data);
     }
 
-    while(1)
+    size_t msgCount = 1;
+    int lastPkg = 1;
+    while (1)
     {
-        printf("Enter message : ");
-        scanf("%s", message);
-
-        if (send(sockedFd, message, strlen(message), 0) < 0)
+        //last package has been sent
+        if (lastPkg)
         {
-            errorLog("Cannot send message\n");
+            printf("Enter message: ");
+            msgCount = 1;
+        }
+
+        if ((fgets(message, BUFLEN, stdin)) != NULL)
+        {
+            //trim character '\n' from the last buffer from stdin
+            if(message[strlen(message) - 1] == '\n')
+            {
+                message[strlen(message) - 1] = '\0';
+                lastPkg = 1;
+            }
+            else
+            {
+                lastPkg = 0;
+            }
+
+            if ((strlen(message) != 0) && ((ret = send(sockedFd, message, strlen(message), 0)) < 0))
+            {
+                errorLog("Cannot send message\n", sockedFd, data);
+            }
+            else if ((strlen(message) != 0))
+            {
+                printf("Package(%lu) has been sent %d bytes of %lu\n", msgCount, ret, strlen(message));
+                msgCount++;
+            }
         }
     }
-
-    close(sockedFd);
-    return 0;
 }
--- Server/main.c
+++ Server/main.c
@@ -1,49 +1,53 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <netinet/in.h>
-#include<unistd.h>
+#include <unistd.h>
 #include <pthread.h>
 
-#define BUFLEN 512
-#define PORT 8888
+#define BUFLEN (512)
+#define PORT (8888)
 
-void errorLog(char *s)
+void errorLogExit(char *s)
 {
     perror(s);
     exit(1);
 }
 
-void *exitOnRequest()
+void *exitOnRequest(void * socket)
 {
-    char input[4];
-    while(strstr(input, "exit") == NULL)
-    {
-        scanf("%s", input);
-    }
+    char input[6];
+    memset(input, 0 , sizeof(input));
 
-    printf("\nServer is shutting down...");
+    do {
+        fgets(input, 7, stdin);
+    }while((strlen(input) != 5) || (strncmp(input, "exit", 4) != 0));
+
+    printf("\nServer is shutting down...\n");
+    close(*((int *)socket));
     exit(0);
 }
 
 int main(void)
 {
     pthread_t tid;
-    pthread_attr_t attr;
 
     struct sockaddr_in server;
+    struct sockaddr_in client;
+    int slen = sizeof(client);
     int sockedFd;
-    char buf[BUFLEN];
-
-    pthread_attr_init(&attr);
-    pthread_create(&tid, &attr, exitOnRequest, NULL);
+    char* buf = malloc(BUFLEN);
+    char* printBuf = NULL;
 
     if ((sockedFd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0)
     {
-        errorLog("Socket creation failed");
+        errorLogExit("Socket creation failed");
     }
 
-    memset((char *) &server, 0, sizeof(server));
+    pthread_create(&tid, NULL, exitOnRequest, (void*) &sockedFd);
+
+    memset(&server, 0, sizeof(server));
 
     server.sin_family = AF_INET;
     server.sin_port = htons(PORT);
@@ -51,20 +55,29 @@ int main(void)
 
     if (bind(sockedFd, (struct sockaddr*)&server, sizeof(server)) < 0)
     {
-        errorLog("Socket is used already");
+        close(sockedFd);
+        errorLogExit("Socket is used already\n");
     }
 
+    memset(buf, 0, BUFLEN);
     printf("Running Server...\n");
+    size_t needed_mem = 0;
+    int ret = 0;
     while(1)
     {
-        if (recv(sockedFd, buf, BUFLEN, 0) < 0)
+        if ((ret = recvfrom(sockedFd, buf, BUFLEN, 0, (struct sockaddr *) &client, &slen)) < 0)
         {
-            errorLog("Receiving package failed");
+            close(sockedFd);
+            errorLogExit("Receiving package failed");
         }
 
-        printf("Received package: %s\n" , buf);
-        memset(&buf[0], 0, sizeof(buf));
+        needed_mem = snprintf(NULL, 0, "%s", buf) + 1;
+        printBuf = realloc(printBuf, needed_mem);
+        snprintf(printBuf, BUFLEN, "%s", buf);
+        printf("Received package: %s\n", buf);
+
+        memset(buf, 0, strlen(buf));
+        memset(printBuf, 0, strlen(printBuf));
     }
-    close(sockedFd);
     return 0;
 }
